da0fb93796f3b3f0e4d4e9d09f63724f22794021
4rturd13 <4rturd13@gmail.com>
2020-09-21 08:25:48 -0500

    first commit

---
+++ src/js/api.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  const btnSwitch = document.querySelector(".switch");
+
+  btnSwitch.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    btnSwitch.classList.toggle("active");
+  });
+});
+
+const formGallery = document.querySelector("#form-search");
+formGallery.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const getData = new FormData(formGallery);
+  search = getData.get("search");
+  getImagesNasa(search);
+});
+
+/* Nasa images api */
+const getImagesNasa = (search) => {
+  const sectionImages = document.querySelector("#section-images");
+  const urlNasaImg = `
+  https://images-api.nasa.gov/search?q=${search}&media_type=image&year_start=2020&year_end=2020`;
+  fetch(urlNasaImg)
+    .then((data) => data.json())
+    .then((data) => {
+      let responseOne = data.collection.items;
+      let countOne = 0;
+      let acumOne = "";
+      responseOne.forEach((item) => {
+        countOne++;
+        let image = item.links[0].href;
+        let title = item.data[0].title;
+        return (acumOne += `
+        <div class="polaroid-gallery"><img class="image-polaroid" src="${image}" alt="image apod"
+        />
+        <p class="copyright-gallery">${title || "No data available"}</p>
+        </div>
+          `);
+      });
+      sectionImages.innerHTML = acumOne;
+    })
+    .catch((error) => error);
+};
+
+const searchParams = "neowise";
+getImagesNasa(searchParams);
---
+++ src/js/behavior.js
@@ -0,0 +1,98 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  const btnSwitch = document.querySelector(".switch");
+
+  btnSwitch.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    btnSwitch.classList.toggle("active");
+  });
+});
+
+/* Disqus API */
+(function () {
+  // DON'T EDIT BELOW THIS LINE
+  var d = document,
+    s = d.createElement("script");
+  s.src = "https://neowise.disqus.com/embed.js";
+  s.setAttribute("data-timestamp", +new Date());
+  (d.head || d.body).appendChild(s);
+})();
+
+const form = document.querySelector("#form");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const getData = new FormData(form);
+  date = getData.get("date");
+  getDataNasa(date);
+});
+
+const youTubeApiKey = "AIzaSyB8fs3-LxKbmoNvgknMisH-bgdXfpGzlUw";
+const channelId = "UCLA_DiR1FfKNvjuUpBHmylQ";
+const totalResult = 4;
+const urlYouTubeApi = `https://www.googleapis.com/youtube/v3/search?key=${youTubeApiKey}&channelId=${channelId}&part=snippet,id&order=date&maxResults=${totalResult}`;
+
+const getDataNasa = (date) => {
+  const sectionOne = document.querySelector("#section-1");
+  const sectionTwo = document.querySelector("#section-2");
+
+  const keyAdpodApi = "ezutVdJrMW4WzgfuAw1CeMDssN5KtBbAW6flgEw7";
+  const urlApodApi = `https://api.nasa.gov/planetary/apod?date=${date}&api_key=${keyAdpodApi}`;
+  fetch(urlApodApi)
+    .then((data) => data.json())
+    .then((data) => {
+      sectionOne.innerHTML = `
+      <h1 class="style-txt title-day">${
+        data.title || "No data available, please choose another date"
+      }</h1>
+      <div class="polaroid"><img class="image-polaroid" src="${
+        data.url
+      }" alt="image apod"
+      />
+      <p class="copyright">${data.copyright || "No data available"}</p>
+      <p class="copyright">${data.date || "No data available"}</p>
+      </div>`;
+      sectionTwo.innerHTML = `
+      <h3 class="subtitle">About Astronomy Picture of the Day (APOD)</h3>
+      <p class="parragraph">${data.explanation || "No data available"}</p>
+      `;
+    })
+    .catch((error) => error);
+};
+
+const getDataYouTube = () => {
+  const sectionThree = document.querySelector("#section-3");
+  fetch(urlYouTubeApi)
+    .then((data) => data.json())
+    .then((data) => {
+      let videos = data.items;
+      let count = 0;
+      let acum = "";
+      videos.forEach((item) => {
+        count++;
+        let video = item.id.videoId;
+        let dataVideo = item.snippet;
+        return (acum += `
+          <div class="youtube-card" id=${count}>
+             <iframe class="youtube-video" src="https://www.youtube.com/embed/${video}" frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+            </iframe>
+            <p class="title-video">${dataVideo.title}</p>
+            <p class="description-video">${dataVideo.description}</p>
+            <div class="align-content">
+            <p class="channel-video">${dataVideo.channelTitle}</p>
+            <p class="publish-time-video">${dataVideo.publishTime}</p>
+            </div>
+          </div>
+      `);
+      });
+      sectionThree.innerHTML = acum;
+    });
+};
+
+const currentDate = new Date();
+let year = currentDate.getFullYear();
+let month = currentDate.getMonth() + 1;
+let day = currentDate.getDate();
+const pickDate = `${year}-${month}-${day}`;
+
+getDataNasa(pickDate);
+getDataYouTube();
